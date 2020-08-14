de02b18f17e2c5b6aecf0db302ba2251fc0ea45d
Prashant Mishra <pgmd.9873@gmail.com>
2020-08-15 01:01:53 +0530

    Added more branches to the Model structure object
    Changed the nested object's list to object to store model properties.

--- main/js/main.js
+++ main/js/main.js
@@ -1,6 +1,7 @@
 var current_layer = 0;
 var layer_counter = 0;
 var lastLayer;
+var numLayers = 0;
 var layer_array = {
     'layers': [
         { 'layer': ['conv2d', 32, 2, 'relu'] },
@@ -15,21 +16,34 @@ var layer_array = {
 var object = {
     "modelName": "myModel",
     "modelType": "Sequential",
-    "modelInfo": ["3"],
+    "modelInfo": ["1"],
     "layers": [{
-        "1": ["Conv2D", "64", "2", "relu"],
-        "2": ["MaxPool2D", "2"],
-        "3": ["Conv2D", "64", "2", "relu"]
+
+        "1": { layerName: "Conv2D", filter: "64", kernel: "2", activation: "relu" }
+
     }]
 };
 
 console.log(object);
 console.log("Model type " + object.modelType + " with " + object.modelInfo + " layers");
 console.log(object.layers);
-for (i = 0; i < parseInt(object.modelInfo); i++) {
-    console.log(String(i + 1) + " layer is of " + object.layers[0][i + 1][0]);
+
+function printModel(object) {
+    for (i = 0; i < parseInt(object.modelInfo); i++) {
+        console.log(String(i + 1) + " layer is of " + object.layers[0][i + 1].layerName);
+    }
 }
 
+function init() {
+    numLayers = 1;
+    console.log("Initiated layer structure with Conv2D layer");
+    printModel(object);
+    console.log("adding maxPool layer");
+    addMaxPoolBlockUi();
+    console.log(object);
+    console.log("new layer structure is:");
+    printModel(object);
+}
 container = $('#add');
 /*
 let layer = document.createElement("div");
@@ -49,6 +63,9 @@ layer.innerHTML =
 container.append(layer);
 */
 function addConvBlockUi() {
+    numLayers += 1;
+    object.layers[0][String(numLayers)] = { layerName: "Conv2D", filter: "64", kernel: "2", activation: "relu" };
+    object.modelInfo = numLayers;
     container.append(`<div class="conv-layer">
     <div class="layer-name">
         <div class="text-box">
@@ -75,6 +92,9 @@ function addConvBlockUi() {
 }
 
 function addMaxPoolBlockUi() {
+    numLayers += 1;
+    object.layers[0][String(numLayers)] = { layerName: "MaxPool2D", kernel: 2 };
+    object.modelInfo = numLayers;
     container.append(`<div class="maxPool-layer">
         <div class="layer-name">
             <div class="text-box">
@@ -96,6 +116,7 @@ function addMaxPoolBlockUi() {
 }
 
 function addFlattenBlockUi() {
+    numLayers += 1;
     container.append(`
         <div class="flatten-layer">
             <div class="layer-name">
@@ -108,6 +129,7 @@ function addFlattenBlockUi() {
 }
 
 function addDenseBlockUi() {
+    numLayers += 1;
     container.append(`
         <div class="dense-layer">
         <div class="layer-name">
@@ -130,21 +152,26 @@ function addDenseBlockUi() {
 }
 
 function addDropoutBlockUi() {
+    numLayers += 1;
     console.log("Build in progress!");
 }
 
 function addBatchNormBlockUi() {
+    numLayers += 1;
     console.log("Build in progress!");
 }
 
 function removeBlockUi() {
+    numLayers -= 1;
     container.remove(lastLayer);
 }
 
 function resetBlockUi() {
+    numLayers += 0;
     container.remove();
 }
 
+// Function for the navigation bar at the top
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -162,3 +189,5 @@ window.onclick = function(event) {
         }
     }
 }
+
+init();
